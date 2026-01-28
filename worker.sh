c8d868b0853ca5249ee1b09591339ef792f6b3ca
Anirudh Rajagopalan <anirudh.raj.iyengar@gmail.com>
2026-01-27 21:32:52 -0800

    update worker sh

--- worker.sh
+++ worker.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 set -e
 
-MASTER_IP="$1"
-
-if [[ -z "$MASTER_IP" ]]; then
-  echo "Usage: ./worker.sh <MASTER_IP>"
+if [ "$#" -lt 1 ]; then
+  echo "Usage: ./worker.sh \"<kubeadm join command>\""
   exit 1
 fi
+
+JOIN_CMD="$@"
 
 echo "[WORKER] Resetting node (safe even if fresh)"
 sudo kubeadm reset -f || true
@@ -14,11 +14,6 @@ sudo rm -rf /etc/cni/net.d ~/.kube || true
 sudo systemctl restart kubelet
 
 echo "[WORKER] Joining cluster"
-sudo kubeadm join ${MASTER_IP}:6443 \
-  --token $(ssh ${MASTER_IP} "kubeadm token list | tail -n1 | awk '{print \$1}'") \
-  --discovery-token-ca-cert-hash \
-  sha256:$(ssh ${MASTER_IP} "openssl x509 -pubkey -in /etc/kubernetes/pki/ca.crt \
-    | openssl rsa -pubin -outform der 2>/dev/null \
-    | sha256sum | awk '{print \$1}'")
+sudo ${JOIN_CMD}
 
 echo "[WORKER] Join complete"
